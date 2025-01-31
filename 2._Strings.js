dbdc0399c49074013ee922b202240491595476f8
Lars <144362461+laha0006@users.noreply.github.com>
2025-01-31 19:34:04 +0100

    solved 2._Strings.js

--- 2._Strings.js
+++ 2._Strings.js
@@ -7,6 +7,8 @@ const numberTwo = "2.30";
 // add those two numbers and show the result
 // you cannot touch line 1 neither line 2
 
+const sum = +numberOne + +numberTwo;
+console.log(sum);
 
 // --------------------------------------
 // Exercise 4 - Add the numbers and the total with 2 decimals
@@ -14,6 +16,8 @@ const numberTwo = "2.30";
 const anotherNumberOne = "1.10";
 const anotherNumberTwo = "2.30";
 
+const anotherSum = +anotherNumberOne + +anotherNumberTwo;
+console.log(anotherSum.toFixed(2));
 
 // --------------------------------------
 // Exercise 5 - Decimals and average
@@ -22,9 +26,14 @@ const one = 10;
 const two = 45;
 const three = 98;
 
+
 // Show in the console the avg. with 5 decimals
 
+const sumOfThree = one + two + three;
+const average = sumOfThree/3;
+const numOfDecimals = 5
 
+console.log(average.toFixed(numOfDecimals));
 
 
 // --------------------------------------
@@ -33,7 +42,8 @@ const three = 98;
 const letters = "abc";
 // Get me the character "c"
 
-
+const c = letters.charAt(2);
+console.log(c);
 
 
 // --------------------------------------
@@ -42,6 +52,9 @@ const letters = "abc";
 const fact = "You are learning javascript!";
 
 // capitalize the J in Javascript
+
+const factWithCaptialJ = fact.replace("j", "J");
+console.log(factWithCaptialJ);
 
 
 // --------------------------------------
